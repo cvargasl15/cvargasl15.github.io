6d481bfd0906b957b86efdd70dcc8d911f001b52
Carlos Vargas <cvargasl15@unc.edu.pe>
2020-10-03 10:49:57 -0500

    commit

--- Laboratorio07/scripts/lab07.js
+++ Laboratorio07/scripts/lab07.js
@@ -1,13 +1,45 @@
 let flag = 1;
+let swcolor = 1;
 
-function changeImage() {
-    let pokeballImg = document.getElementById("icon01");
-    if (flag) {
-        pokeballImg.src = "images/yter.jpg";
-        flag = 0;
+function ex() {
+    alert("It's gone");
+}
+
+function colorear() {
+    if (swcolor) {
+        cambio2();
+        swcolor = 0;
     } else {
-        pokeballImg.src = "images/pokeball.png";
-        flag = 1;
+        cambio1();
+        swcolor = 1;
+    }
+}
+
+function cambio1() {
+    document.getElementById('btn_colorea').style.backgroundColor = '#30629c';
+    var blues = document.getElementsByClassName("blue");
+    for (var i = 0; i < blues.length; i++) {
+        blues[i].style.backgroundColor = '#FF5E00';
+    }
+
+    var oranges = document.getElementsByClassName("orange");
+    for (var i = 0; i < oranges.length; i++) {
+        oranges[i].style.backgroundColor = '#30629c';
     }
 
 }
+
+function cambio2() {
+    document.getElementById('btn_colorea').style.backgroundColor = '#FF5E00';
+    var oranges = document.getElementsByClassName('blue');
+    for (var i = 0; i < oranges.length; i++) {
+        oranges[i].style.backgroundColor = '#30629c';
+    }
+
+    var blues = document.getElementsByClassName('orange');
+    for (var i = 0; i < blues.length; i++) {
+        blues[i].style.backgroundColor = '#FF5E00';
+    }
+}
+
+window.onload = colorear;
